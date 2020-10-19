0d9bc6bd9abe8d5a62099c6ee5ec7829e64c0136
kovs705 <smartkovsik@gmail.com>
2020-10-19 21:03:56 +0300

    added some lines

--- JapHelper/ContentView.swift
+++ JapHelper/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     lastAndSearch()
+                    VocabularyAdd()
                 }
             }
             .navigationTitle("Japanese")
--- JapHelper/SubViews/VocabularyAdd.swift
+++ JapHelper/SubViews/VocabularyAdd.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct VocabularyAdd: View {
+    
+    let quickList = ["Adjectives", "Verbs", "Nouns"]
+    
     // quick list of your vocabulary + abbility to create the new list (or group)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Vocabulary")
+                    .font(.system(size: 23))
+                
+                Spacer()
+                
+                Button(action: {
+                    // create a list with alert:
+                    
+                }) {
+                    Image(systemName: "plus")
+                        .font(.system(size: 24))
+                        .foregroundColor(.red)
+                }
+            }
+            .padding()
+            // end of HStack
+            List {
+                ForEach(quickList, id: (\.self)) { item in
+                    HStack {
+                        Text("\(item.description)")
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
